c431a62bdccfac3a01959118ac89fc4e3a38718a
Moustafa-Ashraf04 <moustafa.ashraf277@gmail.com>
2025-06-14 13:48:28 +0300

    add contact us page & content adjustments in home page

--- src/app/pages/public/contact/contact.component.ts
+++ src/app/pages/public/contact/contact.component.ts
@@ -1,11 +1,30 @@
 import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './contact.component.html',
-  styleUrl: './contact.component.css'
+  styleUrl: './contact.component.css',
 })
 export class ContactComponent {
+  contactForm = {
+    name: '',
+    email: '',
+    phone: '',
+    country: '',
+    message: '',
+  };
 
+  onSubmit(form: NgForm) {
+    if (form.valid) {
+      // Here you would typically send the form data to your server
+      console.log('Form submitted:', form.value);
+      // Reset the form after submission
+      form.reset();
+    } else {
+      console.log('Form is invalid');
+      console.log('Form errors:', form);
+    }
+  }
 }
