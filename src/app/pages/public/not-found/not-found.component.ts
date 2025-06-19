79f8d0b786f8832f22bfa14cc84788a0e4e4b7aa
Moustafa-Ashraf04 <moustafa.ashraf277@gmail.com>
2025-06-19 14:07:05 +0300

    feat: added 404 page v1

--- src/app/pages/public/not-found/not-found.component.ts
+++ src/app/pages/public/not-found/not-found.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-not-found',
-  imports: [],
+  imports: [RouterModule, CommonModule],
   templateUrl: './not-found.component.html',
   styleUrl: './not-found.component.css',
 })
